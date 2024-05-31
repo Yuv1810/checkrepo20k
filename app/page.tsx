a1d1dd65be81d96d8524ab3df2444a971a5d3e95
Yuvraj <engyuvrajsingh@gmail.com>
2024-05-31 22:08:17 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,179 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useRef } from 'react';
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+ 
+const ReactMarkdown = dynamic(() => import('react-markdown'), {
+  ssr: false,
+})
+
+
+
+const markdown = `# Recipe App
+
+Welcome to the Recipe App! This application allows users to create, share, and organize their favorite recipes into categories. Whether you're a seasoned chef or a cooking enthusiast, this app will help you keep your recipes organized and accessible.
+
+## Features
+
+1. **Create Recipes**: Users can create their own recipes by providing a recipe name, ingredients list, cooking instructions, and optional details like cooking time, serving size, and dietary notes.
+
+2. **Categorize Recipes**: Users can organize their recipes into different categories such as breakfast, lunch, dinner, desserts, etc.
+
+3. **Responsive Design**: The app is designed to be responsive, ensuring a seamless experience across various devices, including desktops, tablets, and smartphones.
+
+
+![WhatsApp Image 2023-08-24 at 16 23 41](https://github.com/Yuv1810/recipe-app/assets/119923256/e61fb2d3-f0aa-49a4-ae8e-09a5043aaa86)
+![WhatsApp Image 2023-08-24 at 16 24 21](https://github.com/Yuv1810/recipe-app/assets/119923256/12f8c86a-08b3-4941-9e1b-bf9c93114dca)
+![WhatsApp Image 2023-08-24 at 16 28 13](https://github.com/Yuv1810![WhatsApp Image 2023-08-24 at 16 30 02](https://github.com/Yuv1810/recipe-app/assets/119923256/0bbd06ab-a2b7-4bea-8601-35e7d693969f)
+/recipe-app/assets/119923256/6714da1b-b144-4664-87cd-7bff3f62d6ea)
+![WhatsApp Image 2023-08-24 at 16 29 16](https://github.com/Yuv1810/recipe-app/assets/119923256/26f6cf9d-f436-49fb-9976-3b3275673ddd)
+![WhatsApp Image 2023-08-24 at 16 46 32](https://github.com/Yuv1810/recipe-app/assets/119923256/7aa30870-26d3-47d9-a95f-fd5d0ccf1d1d)
+![WhatsApp Image 2023-08-24 at 16 46 33 (1)](https://github.com/Yuv1810/recipe-app/assets/119923256/c9b95f2b-83b0-4809-af36-8f75056044bc)
+![WhatsApp Image 2023-08-24 at 16 46 33 (2)](https://github.com/Yuv1810/recipe-app/assets/119923256/ae200c1c-9530-4aa7-b90a-28e548045738)
+![WhatsApp Image 2023-08-24 at 16 46 33](https://github.com/Yuv1810/recipe-app/assets/119923256/ac9d9c63-0732-45ba-b9a4-678c305ad2f8)
+`
+
+
+const DynamicTextarea = () => {
+  const [text,setetext]=useState('');
+  const [visible,setvisible]=useState(false);
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const [user,setuser]=useState(['']);
+  const [conversation,setcoversation]=useState<string[]>([]);
+
+  useEffect(() => {
+    const textarea = textareaRef.current;
+
+    const resizeTextarea = () => {
+      textarea!.style.height = 'auto';
+      textarea!.style.height = `${textarea!.scrollHeight}px`;
+    };
+
+    textarea!.addEventListener('input', resizeTextarea);
+
+    return () => {
+      textarea!.removeEventListener('input', resizeTextarea);
+    };
+  }, []);
+
+  return (
+    <>
+    <div className='flex h-screen w-screen justify-center'>
+
+    
+  
+      {(visible) ? <div className='bg-white text-blue md:w-1/6 h-full fixed z-10 start-0 w-60 overflow-scroll'> 
+      <button className='bg-black w-20' onClick={(visible)=>{
+        setvisible(!visible);
+      }}>stj</button>
+
+      <div className='bg-black h-20'>hey</div>
+      <div className='bg-black h-20'>hey</div>
+      <div className='bg-black h-20'>hey</div>
+      <div className='bg-black h-20'>hey</div>
+      
+      <div className='bg-black h-20'>hey</div>
+      <div className='bg-black h-20'>hey</div>
+      <div className='bg-black h-20'>hey</div>
+      <div className='bg-black h-20'>hey</div> <div className='bg-black h-20'>hey</div>
+      <div className='bg-black h-20'>hey</div>
+      <div className='bg-black h-20'>hey</div>
+      <div className='bg-black h-20'>hey</div>
+      <div className='bg-black h-20'>hey</div>
+      <div className='bg-black h-20'>hey</div>
+      
+
+      </div>
+      :
+      // <div className='bg-white text-blue h-full fixed z-100 start-0'> 
+      <button className='bg-black w-20 fixed z-100 start-0' onClick={(visible)=>{
+        setvisible(true);
+      }}>stj</button>
+      // </div>
+      }
+      
+   
+
+    
+    <div className='flex flex-col items-center w-5/6 md:ml-52 h-full'>
+
+   
+    <div className='h-5/6 w-5/6 mt-10 overflow-x-none'>
+      
+
+   
+   
+{conversation.map((d)=>{
+  return <>
+  <ReactMarkdown>{d}</ReactMarkdown>
+  <div className='w-full h-px bg-gray-300 rounded-md mt-4 mb-4'></div>
+  </>
+})}
+
+      <div className='h-32 w-full'></div>
+
+    </div>
+   
+
+
+      <div className='flex justify-around fixed z-100 bottom-1 md:w-3/6 w-80'>
+     <textarea
+      ref={textareaRef}
+      value={text}
+      placeholder="Type Here..."
+      className="w-full p-2 border border-gray-300 rounded resize-none outline-none overflow-scroll text-black max-h-32 w-1/2 border-2"
+      onChange={((e)=>{
+        setetext(e.target.value);
+      })}
+    ></textarea>
+    <button className='bg-white w-20 h-10 text-black rounded-lg border-2 border-gray-300 ml-4 mt-4 mr-4' onClick={async ()=>{
+      // call the api
+       setTimeout(() => {
+        console.log("Fetching data");
+      }, 2000);
+      // then setstate of conversation
+      var arr=[...conversation];
+      arr.push(text);
+      arr.push(markdown);
+      setcoversation(arr);
+     
+    }}>send</button>
+      </div>
+      
+
+
+
+
+
+   
+     </div>
+   </div>
+    </>
+   
+  );
+};
+
+
+
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+<>
+    
+     
+      <DynamicTextarea />
+     
+   
+   
+     
+   
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+</>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
 }
